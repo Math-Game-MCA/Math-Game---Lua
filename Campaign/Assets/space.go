15c03ac99246d5e132683fed62c645ff07dee988
Lucent Fong <lucent.fong@gmail.com>
2017-08-06 20:20:13 -0400

    Campaign Assets
    Added a Campaign Atlas and folder as an overall structure to set up the
    new campaign feature

---
+++ Campaign/Assets/space.go
@@ -0,0 +1,60 @@
+embedded_components {
+  id: "space"
+  type: "sprite"
+  data: "tile_set: \"/Main/campaign.atlas\"\n"
+  "default_animation: \"campaign_background\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.5
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "background"
+  type: "sprite"
+  data: "tile_set: \"/Main/campaign.atlas\"\n"
+  "default_animation: \"background_faded\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "glow"
+  type: "sprite"
+  data: "tile_set: \"/Main/campaign.atlas\"\n"
+  "default_animation: \"glow\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.7
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
